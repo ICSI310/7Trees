5c8b7a1d23fc9621cb07449ba62a0adcb71589d5
cmagnus <cmagnus@gmail.com>
2016-04-21 13:26:30 -0400

    Added outline of code so far

--- Main.java
+++ Main.java
@@ -8,7 +8,6 @@ public class Main
   public static void main(String[] args) throws IOException{
     //MyList list = new MyList();
     //Double elapsedTime = new Double(0);// assume 0 = first hero captured
-    double elapsedTime = 0;
     Scanner input = null;
 
         /*try {
